bafc66faf46f9f663d52f1d4f4cb2a4a04a77027
chenlianghong <clh021@gmail.com>
2016-09-09 02:55:21 +0000

    show database initialization in green font message

--- dockerfiles/ozzo/README.md
+++ dockerfiles/ozzo/README.md
@@ -6,3 +6,7 @@ docker run -it --rm -p 8080:8080 --name restful --link postgres:postgres leehom/
 docker run -it -p 8080:8080 --name restful --link postgres:postgres leehom/ozzo-restful-kit bash #调试使用
 ```
 境内用户如需包含更多包，欢迎反馈，会酌情添加
+
+#细节
+系统根目录的 `start.sh` 脚本会根据项目根目录的 `db.init.lock` 文件来判断是否需要执行初始化操作
+如需重新进行初始化操作，删掉 根目录的 `db.init.lock` 即可
--- dockerfiles/ozzo/start.sh
+++ dockerfiles/ozzo/start.sh
@@ -2,7 +2,7 @@
 cd $GOPATH/src/github.com/qiangxue/golang-restful-starter-kit/
 LOCKFILE=./db.init.lock
 if [ -f $LOCKFILE ]; then
-   	echo "Database had been inited"
+    echo -e "\033[32mDatabase has been initialized\033[0m"
 else
     apt-get update
     apt-get install pwgen
@@ -10,6 +10,7 @@ else
     echo "jwt_verification_key: \"$(pwgen -s 32 1)\"">>config/app.yaml
     echo "jwt_signing_key: \"$(pwgen -s 32 1)\"">>config/app.yaml
     go run testdata/init.go
-    echo "Database had been inited" > db.init.lock
+    echo -e "\033[32mStart the database initialization\033[0m"
+    echo "1" > db.init.lock
 fi
 go run server.go
